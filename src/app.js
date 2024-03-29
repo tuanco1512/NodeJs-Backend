243a44022d0211ce48b201e6dbade0c022c0e66e
ADMIN <tuanco151220002@gmail.com>
2024-03-29 20:00:08 +0700

    session 1 Node-Js Backend commit

--- src/app.js
+++ src/app.js
@@ -15,14 +15,16 @@ app.use(helmet())
 app.use(compression())
 
 // init db
-
+require('./dbs/init.mongodb')
+const {checkOverload} = require('./helpers/check.connect')
+checkOverload()
 // init routes
 app.get('/',(req,res,next)=>{
     const strCompress = 'Hello Kidman'
 
     return res.status(200).json({
         message: 'Welcome Kidman!',
-        metadata: strCompress.repeat(100000)
+        // metadata: strCompress.repeat(100000)
     })
 })
 
